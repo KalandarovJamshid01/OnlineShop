a12c65906dbad46e823d9761527ea7ddd5431dac
Jamshid <kalandarovjamshid01@gmail.com>
2022-07-28 19:23:44 +0500

    catchError and res Error functions are ready

--- middleware/app.js
+++ middleware/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
+const AppError = require("./../utility/AppError");
+const ErrorController = require("./../utility/ErrorController");
 const app = express();
+
+app.all("*", function (req, res, next) {
+  next(new AppError(`this url hasn't found:${req.originalUrl}`, 404));
+});
+app.use(ErrorController);
 
 module.exports = app;
