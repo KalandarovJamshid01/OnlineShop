02ea83344616f3d89f002cc5e8398ef455ebac2c
Jamshid <kalandarovjamshid01@gmail.com>
2022-08-01 14:57:23 +0500

    okay, not bad

--- controller/viewController.js
+++ controller/viewController.js
@@ -4,11 +4,15 @@ const Product = require("./../model/productModel");
 const Review = require("./../model/reviewModel");
 const ExternalCategory = require("./../model/externalCategoryModel");
 const InternalCategory = require("./../model/internalCategoryModel");
-
+const Service = require("./../model/serviceModel");
 const home = catchErrorAsync(async (req, res, next) => {
-//   const data = await ExternalCategory.find();
+  const externalCategory = await ExternalCategory.find();
+  const product = await Product.find();
+  const review = await Review.find();
+  const internalCategory = await InternalCategory.find();
+  const service = await Service.find();
   res.render("home", {
-    data: "salom",
+    externalCategory: externalCategory,
   });
 });
 
--- model/serviceModel.js
+++ model/serviceModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const serviceScshema = new mongoose.Schema({
+const serviceSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
